29c5967eb2f09b10e5ef67233f32fa43c410fcc4
julian styles <julian.styles@flaschenpost.de>
2025-07-02 12:13:20 +0200

    * added compiled Datasets

---
+++ js/datasets/datasets.js
@@ -0,0 +1,26 @@
+export const mealOptions = [
+    { value: '', text: '-- Auswählen --' },
+    { value: 'infield_food', text: 'Infield Food', exclude: true },
+    { value: 'dosenmahlzeit_gross', text: 'Dosenmahlzeit (groß)' },
+    { value: 'dosenmahlzeit_klein', text: 'Dosenmahlzeit (klein)' },
+    { value: 'instantnudeln_gross', text: 'Instantnudeln (groß)' },
+    { value: 'instantnudeln_klein', text: 'Instantnudeln (klein)' },
+    { value: 'bratkartoffeln', text: 'Bratkartoffeln' },
+    { value: 'chips', text: 'Chips' },
+    { value: 'nuesse', text: 'Nüsse' },
+    { value: 'kekse', text: 'Kekse' },
+    { value: 'proteinbars', text: 'Proteinbars' }
+];
+export const secondaryOptions = [
+    { value: '', text: '-- Zusätzliche Auswahl --' },
+    { value: 'infield_food', text: 'Infield Food', exclude: true },
+    { value: 'dosenmahlzeit_gross', text: 'Dosenmahlzeit (groß)' },
+    { value: 'dosenmahlzeit_klein', text: 'Dosenmahlzeit (klein)' },
+    { value: 'instantnudeln_gross', text: 'Instantnudeln (groß)' },
+    { value: 'instantnudeln_klein', text: 'Instantnudeln (klein)' },
+    { value: 'bratkartoffeln', text: 'Bratkartoffeln' },
+    { value: 'chips', text: 'Chips' },
+    { value: 'nuesse', text: 'Nüsse' },
+    { value: 'kekse', text: 'Kekse' },
+    { value: 'proteinbars', text: 'Proteinbars' }
+];
